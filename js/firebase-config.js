256a0cf1cb3cd784e7f5db743011bb655e3714b3
faicelmaths20 <faicelmaths@gmail.com>
2026-04-28 21:39:41 +0100

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBx8_xMkIgAc2CDNSGjoPn7zxCklMvZKcs",
+  authDomain: "missaouimaths.firebaseapp.com",
+  projectId: "missaouimaths",
+  storageBucket: "missaouimaths.firebasestorage.app",
+  messagingSenderId: "781971966091",
+  appId: "1:781971966091:web:d5522f8d70b008a844ac8e"
+};
+export { firebaseConfig };
